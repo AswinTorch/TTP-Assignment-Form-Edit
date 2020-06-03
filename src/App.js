b8f301d58736d44139acf2296607e2aa30250159
Aswin Nair <aswintorch@gmail.com>
2020-06-03 10:57:32 -0400

    Added edit and cancel toggle to conditionally display divs or form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "Aswin",
+      lastName: "Nair",
+      editClicked: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState((state) => ({
+      editClicked: !state.editClicked,
+    }));
+  };
+
+  render() {
+    let display = !this.state.editClicked ? (
+      <>
+        <div>First Name: {this.state.firstName}</div>
+        <div>Last Name: {this.state.lastName}</div> <br />
+        <button onClick={this.handleClick}>Edit</button>
+      </>
+    ) : (
+      <>
+        <form action="">
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+          />
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+          />
+          <button onClick={this.handleClick}>Cancel</button>
+        </form>
+      </>
+    );
+
+    return (
+      <div className="App">
+        <h1>Form Edit</h1>
+        <div>{display}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
