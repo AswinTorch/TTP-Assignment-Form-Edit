29e5c285cadc3c242020ff31f168e8bd56bf3e6c
Aswin Nair <aswintorch@gmail.com>
2020-06-03 11:21:41 -0400

    Added functional saving of data and canceling of the form with original state intact

--- src/App.js
+++ src/App.js
@@ -9,20 +9,49 @@ class App extends Component {
       lastName: "Nair",
       editClicked: false,
     };
+
+    this.originalState = this.state;
   }
 
-  handleClick = () => {
+  // Helper function to toggle view
+  toggle = () => {
     this.setState((state) => ({
       editClicked: !state.editClicked,
     }));
   };
 
+  // Edit button leads to form to edit data
+  edit = () => {
+    this.toggle();
+  };
+
+  // Cancels edit mode and keeps original data
+  cancel = () => {
+    this.setState({ ...this.originalState });
+    this.toggle();
+  };
+
+  // Saves the updated data
+  save = () => {
+    this.originalState = this.state;
+    this.toggle();
+  };
+
+  // Form input handler function
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    // Conditional rendering based on button click state
     let display = !this.state.editClicked ? (
       <>
         <div>First Name: {this.state.firstName}</div>
         <div>Last Name: {this.state.lastName}</div> <br />
-        <button onClick={this.handleClick}>Edit</button>
+        <button onClick={this.edit}>Edit</button>
       </>
     ) : (
       <>
@@ -31,15 +60,18 @@ class App extends Component {
             type="text"
             name="firstName"
             id="firstName"
-            placeholder="First Name"
+            value={this.state.firstName}
+            onChange={this.handleChange}
           />
           <input
             type="text"
             name="lastName"
             id="lastName"
-            placeholder="Last Name"
+            value={this.state.lastName}
+            onChange={this.handleChange}
           />
-          <button onClick={this.handleClick}>Cancel</button>
+          <button onClick={this.cancel}>Cancel</button>
+          <button onClick={this.save}>Save</button>
         </form>
       </>
     );
